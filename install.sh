fcd33cd308b08bee5ee0a0148dc7b9f9782daf95
ksch12052000ksu-cyber <ksch12052000ksu@gmail.com>
2026-05-06 16:18:20 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+apt update && apt install -y git curl openssl qrencode systemd
+
+REPO_DIR="$HOME/self-signed-cert-script-by-antenka"
+
+if [ -d "$REPO_DIR" ]; then
+    cd "$REPO_DIR" && git pull
+else
+    git clone https://github.com/anten-ka/self-signed-cert-script-by-antenka.git "$REPO_DIR"
+fi
+
+cd "$REPO_DIR"
+chmod +x self_signed_cert.sh
+./self_signed_cert.sh
